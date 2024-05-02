dba15177f8a937e41b92018e245f66f8261c8a46
Rhaíssa Lima <rhaissasbgl@gmail.com>
2024-05-02 11:27:09 -0300

    fetch animais

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ import initDropdown from "./modules/dropdown.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import initAnimaNumeros from "./modules/anima-numeros.js";
 import initFuncionamento from "./modules/funcionamento.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 
 initScrollSuave();
 initAnimacaoScroll();
@@ -17,5 +18,5 @@ initModal();
 initTooltip();
 initDropdown();
 initMenuMobile();
-initAnimaNumeros();
 initFuncionamento();
+initFetchAnimais();
